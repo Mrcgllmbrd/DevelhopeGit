2b6fb8b3f73d2686c12b6a5c31c75b01431c5c59
MarcoG <baldogiallo@gmail.com>
2023-09-12 16:28:32 +0200

    Javascript

--- Javascript/exercise5/exercise.js
+++ Javascript/exercise5/exercise.js
@@ -4,9 +4,16 @@ const person1 = {
   age: 25,
 };
 
-const person2 = person1;
+let person2 = person1;
+person2 = Object.assign({}, person1);
+person2.firstName= 'Simon'
 
 // Modify the property `firstName` of the `person2` in "Simon".
 
 console.log(person1);
 console.log(person2);
+/* 
+SPIEGAZIONE: iL motivo per cui dichiarando una nuova variabile person2 anche person1 
+viene modificato è che stiamo puntando verso il primo oggetto, quindi non stiamo creando una copia separata,
+ bensì stiamo ridefinendo i valori di person1.
+ */
