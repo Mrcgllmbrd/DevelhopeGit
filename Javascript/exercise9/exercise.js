6da76dd1bdcc03ec53315313e0a814e324a4c987
MarcoG <baldogiallo@gmail.com>
2023-09-19 13:12:27 +0200

    Javascript

--- Javascript/exercise9/exercise.js
+++ Javascript/exercise9/exercise.js
@@ -1,12 +1,11 @@
 const person = {
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-}
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
 
 const keys = Object.keys(person);
 
 for (key of keys) {
   console.log(`${key}: ${person[key]}`);
 }
-
