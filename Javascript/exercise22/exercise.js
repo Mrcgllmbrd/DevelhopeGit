9549681587180169128edd7bdc785e4558a6fef2
MarcoG <baldogiallo@gmail.com>
2023-09-25 23:45:41 +0200

    Javascript

--- Javascript/exercise22/exercise.js
+++ Javascript/exercise22/exercise.js
@@ -1,13 +1,15 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
 };
 
-const id = person.id;
+const { id: id, firstName: firstName, lastName: lastName, age: age } = person;
+
+/* const id = person.id;
 const firstName = person.firstName;
 const lastName = person.lastName;
-const age = person.age;
+const age = person.age; */
 
 console.log(id, firstName, lastName, age);
