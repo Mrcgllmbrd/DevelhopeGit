264c70687837045df61a8c71c96168236ed49607
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-10-19 19:35:08 +0200

    javascript

--- Javascript/exercise16/exercise.js
+++ Javascript/exercise16/exercise.js
@@ -4,7 +4,9 @@ class Person {
         this.firstName= firstName;
         this.lastName= lastName;
     }
+
 }
+
 
 const developer = new Person('Mario', 'Rossi');
 console.log(developer.firstName + " " + developer.lastName);
