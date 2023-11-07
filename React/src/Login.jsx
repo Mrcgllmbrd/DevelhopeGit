48f4a33412d76007ce3a9203612488fbf1a0fa8b
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-11-07 20:11:17 +0100

    Controlled-forms-4

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -25,6 +25,17 @@ export function Login({ onLogin }) {
     onLogin(data.username);
   }
 
+  function handleReset() {
+      setData ({
+        username: "",
+        password: "",
+        session: false,
+    });
+
+  }
+
+
+
   return (
     <div>
       <label> User </label>
@@ -53,12 +64,11 @@ export function Login({ onLogin }) {
        onClick={handleBtn}>
         Login
       </button>
+
+      <button onClick={handleReset}> Reset </button>
     </div>
   );
 }
 
-/* Add a "login" button to the Login component. 
-This button should be disabled as long as the username and password inputs are empty.
-When clicked, the event handler attached to the button 
-should call an onLogin function passed as a prop
-to the Login component, passing it the state, that will print the state value. */
+/* Add a "reset" button to the Login component
+ that clears the content of all three inputs when clicked. */
