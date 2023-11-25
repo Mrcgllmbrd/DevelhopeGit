27d66aae14ddc967b1a4ad463657f643065622a2
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-11-25 22:30:02 +0100

    React Router 4

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,22 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 export function App() {
   return (
     <div>
       <BrowserRouter>
+        <h1>my awesome App</h1>
+        <div>
+          <Link to="/"> Welcome </Link> |
+          <Link to="/:username"> User </Link> |
+          <Link to="/counter"> Counter </Link> |
+        </div>
         <Routes>
+          <Route path="/" element={<Welcome />} />
           <Route path="/:username" element={<ShowGithubUser />} />
+          <Route path="/counter" element={<Counter />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-/* 
-Add a new Route to the users/:username path
-that renders a ShowGithubUser 
-component that receives the username as a path parameter
-and renders the GithubUser
-component from the first Data Fetching
-with fetch and useEffect exercise by passing it the received username. 
+/*
+Add three Links within the main App component and use them to navigate to all three routes.
 */
---
+++ React/src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export function Counter({ initialValue, incrementAmount }) {
+  const [counter, setCounter] = useState(initialValue);
+
+  function handleCountIncrement() {
+    setCounter(counter + incrementAmount);
+  }
+
+  return (
+    <div>
+      <h2> the count is: {counter} </h2>
+      <button onClick={handleCountIncrement}> Increment </button>
+    </div>
+  );
+}
