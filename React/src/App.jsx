b068bc3836c226e879f191fd99b32628c8ba8acc
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-11-26 14:04:11 +0100

    React-Router-5

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { GithubUserList } from "./GithubUserList";
 import { ShowGithubUser } from "./ShowGithubUser";
 
@@ -9,17 +9,20 @@ export function App() {
         <h1>my awesome App</h1>
 
         <Routes>
-          <Route path="/" element={<GithubUserList />} />
-          <Route path="/users/:username" element={<ShowGithubUser />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <p>user not found</p>
+                <Link to="/users"> back to home </Link>
+              </div>
+            }
+          />
+          <Route path="/users" element={<GithubUserList />} >
+           <Route path=":username" element={<ShowGithubUser />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-/*
-Create a GithubUserList component that fetching this API url,
- https://api.github.com/users, show a list of links of github usernames.
-By clicking on a username, the ShowGithubUser component will be displayed.
-Add a Route to the users path that shows the GithubUserList component.
-In doing so, remove the /users/:username route from the App component,
-and add a new nested route within the /users route.*/
--- React/src/ShowGithubUser.jsx
+++ React/src/ShowGithubUser.jsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 export function ShowGithubUser() {
   const { username } = useParams();
 
-    console.log(username);
 
   return <div> 
     <h3> user: {username}</h3>
