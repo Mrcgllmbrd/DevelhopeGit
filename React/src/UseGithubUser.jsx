7c254e835e3f54b6d7ec95208e3b0a39bccfb87b
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-11-29 21:46:42 +0100

    SWR 2

--- React/src/UseGithubUser.jsx
+++ React/src/UseGithubUser.jsx
@@ -3,15 +3,27 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function UseGithubUser({ username }) {
+
+ 
+
   const { data, error } = useSWR(
     `https://api.github.com/users/${username}`,
     fetcher
   );
-  console.log({ data });
-  console.log({error})
+   if (username === null) {
+     return {
+       data: null,
+     } 
+   }
 
-  return {
+   return {
     data,
-    error,
-  };
+    error
+   }
+  
 }
+
+/*
+Modify the useGithubUser hook so that,
+if the username is null, no request is made. 
+*/
