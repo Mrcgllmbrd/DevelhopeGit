a04abdc1ddc6b48a23a6a4c874cb1b1375ec52c0
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-12-07 13:07:30 +0100

    Typescript

--- Typescript/5-any-unknown-never-types/exercise-5-any-unknown-never-types-start.ts
+++ Typescript/5-any-unknown-never-types/exercise-5-any-unknown-never-types-start.ts
@@ -3,7 +3,7 @@
  */
 
 // Add the `any` type to fix the type errors in the following code.
-let currency:any = { name: "Indian rupee" };
+let currency: any = { name: "Indian rupee" };
 
 currency.code = "THB";
 
@@ -12,14 +12,10 @@ currency = "Baht";
 // Fix the if statement in this function so the type of `value` is narrowed to `string`.
 
 function countryNameLength(value: unknown) {
-  if (typeof value === "number") {
-    // Convert 'value' to 'string' only if it's a number
-    const stringValue: string = String(value);
-    console.log(stringValue.length);
+  if (typeof value === "string") {
+    console.log(value.length);
   }
 }
-
-
 
 countryNameLength("United States of America");
 
@@ -27,7 +23,7 @@ countryNameLength("United States of America");
 // Hint: Replace one of the `never` types with a different type.
 
 function throwCountryError(message: string): never {
-    throw new Error(`Could not find country: ${message}`);
+  throw new Error(`Could not find country: ${message}`);
 }
 
 throwCountryError("Narnia");
--- Typescript/8-extend-an-interface/exercise-3-extend-an-interface-start.ts
+++ Typescript/8-extend-an-interface/exercise-3-extend-an-interface-start.ts
@@ -2,7 +2,7 @@
  * START: Follow the instructions below.
  */
 
-interface Country {
+interface Country  {
     name: string;
     code: string;
 }
@@ -12,8 +12,8 @@ interface Country {
 // - Extend the `Country` interface
 // - Add a `population` property with an appropriate type
 
-interface CountryWithPopulation {
-
+interface CountryWithPopulation extends Country {
+population: number;
 }
 
 const countryA: CountryWithPopulation = {
