850e8273e3a422e9d91f33ff6719af949a28395e
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-12-30 10:10:22 +0100

    Node

--- Node/11-Listen to the news/news.js
+++ Node/11-Listen to the news/news.js
@@ -1,5 +1,5 @@
-/* The newsEvent object continuously emits three different events: newsEvent, breakingNews and error
-
+/* The newsEvent object continuously emits three different events: 
+newsEvent, breakingNews and error
 Attach event listeners for each event and log out their data. */
 
 const { EventEmitter } = require("node:events");
